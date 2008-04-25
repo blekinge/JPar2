235bf479bd6ec7b70f3fcf5d938de39acf848608
abr <abr@1d777716-764b-0410-8a5b-8316bd271108>
2008-04-25 13:12:22 +0000

    Reed solomon

---
+++ test/unit/src/dk/statsbiblioteket/jpar2/reedsolomon/math/field/GaloisFieldTest.java
@@ -0,0 +1,100 @@
+/*
+ *     GaloisFieldTest.java
+ *     Copyright (C) 2008  Asger Blekinge-Rasmussen
+ * 
+ *     This library is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU Lesser General Public
+ *     License as published by the Free Software Foundation; either
+ *     version 2.1 of the License, or (at your option) any later version.
+ * 
+ *     This library is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *     Lesser General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Lesser General Public
+ *     License along with this library; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package dk.statsbiblioteket.jpar2.reedsolomon.math.field;
+
+import dk.statsbiblioteket.jpar2.reedsolomon.math.field.GaloisField;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Asger Blekinge-Rasmussen
+ */
+public class GaloisFieldTest extends TestCase {
+
+    GaloisField field;
+
+    public GaloisFieldTest(String testName) {
+        super(testName);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        field = new GaloisField(GaloisField.WordSize.FOUR);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of div method, of class GaloisField.
+     */
+    public void testDiv() {
+        assertEquals(field.div(13, 10).intValue(), 3);
+        assertEquals(field.div(3, 7).intValue(), 10);
+
+
+    }
+
+    /**
+     * Test of mult method, of class GaloisField.
+     */
+    public void testMult() {
+        assertEquals(field.mult(13, 10).intValue(), 11);
+        assertEquals(field.mult(3, 7).intValue(), 9);
+
+    }
+
+    /**
+     * Test of pow method, of class GaloisField.
+     */
+    public void testPow() {
+        assertEquals(field.pow(4, 3).intValue(), 12);
+        assertEquals(field.pow(3, 2).intValue(), 5);
+        assertEquals(field.pow(9, 3).intValue(), 15);
+
+
+    }
+
+    /**
+     * Test of add method, of class GaloisField.
+     */
+    public void testAdd() {
+        assertEquals(field.add(11, 7).intValue(), 12);
+
+
+    }
+
+    /**
+     * Test of sub method, of class GaloisField.
+     */
+    public void testSub() {
+        assertEquals(field.sub(11, 7).intValue(), 12);
+
+
+    }
+    
+    public void testInvert() {
+        assertEquals(field.div(1,7),field.invert(7));
+        assertEquals(field.div(1,1),field.invert(1));
+        assertEquals(field.div(1,15),field.invert(15));
+    }
+}
---
+++ test/unit/src/dk/statsbiblioteket/jpar2/reedsolomon/math/matrix/RecoveryTest.java
@@ -0,0 +1,132 @@
+/*
+ *     RecoveryTest.java
+ *     Copyright (C) 2008  Asger
+ * 
+ *     This library is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU Lesser General Public
+ *     License as published by the Free Software Foundation; either
+ *     version 2.1 of the License, or (at your option) any later version.
+ * 
+ *     This library is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *     Lesser General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Lesser General Public
+ *     License along with this library; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package dk.statsbiblioteket.jpar2.reedsolomon.math.matrix;
+
+import dk.statsbiblioteket.jpar2.reedsolomon.math.field.Field;
+import dk.statsbiblioteket.jpar2.reedsolomon.math.field.GaloisField;
+import java.util.Arrays;
+import java.util.List;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Asger
+ */
+public class RecoveryTest extends TestCase {
+
+    public RecoveryTest(String arg0) {
+        super(arg0);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testRecovery() throws MatrixDimensionException, MatrixException,
+            NotRecoverableException {
+        //First, we make the data vector
+
+        Field<Integer> field = new GaloisField(GaloisField.WordSize.EIGHT);
+
+        Integer[] matrix = new Integer[]{
+            11,
+            2,
+            3,
+            44,
+            5,
+            6
+        };
+
+
+
+
+        Matrix<Integer, Field<Integer>> data =
+                new Matrix<Integer, Field<Integer>>(matrix.length, 1, field, matrix);
+
+        int recoveryBlocks = 6;
+        
+        //+3 is the number of recovery blocks to make
+        Matrix<Integer, Field<Integer>> disp = new DispersalMatrix(data.getRows() + recoveryBlocks, data.getRows(), field);
+
+        Matrix<Integer, Field<Integer>> parity = Matrix.mult(disp, data, field);
+
+
+
+        List<Boolean> present = Arrays.asList(new Boolean[]{
+            
+            false,//datablocks
+            false,
+            false,
+            true,
+            false,
+            false,
+            
+            true,//recoveryblocks
+            true,
+            true,
+            true,
+            true,
+            true
+        });
+        
+        if (present.size() != disp.getRows()){
+            fail("The present list must account for all the rows in the dispersal matrix");
+        }
+
+        CondensedDispersalMatrix c = new CondensedDispersalMatrix(data.getRows(), data.getRows(), disp, present);
+        int[] id = c.getRowMapping();
+        
+        Matrix<Integer, Field<Integer>> cond = c.invert();
+
+
+
+        
+
+        Integer[] matrix2 = new Integer[data.getRows()];
+        
+        for (int i=0; i<matrix2.length; i++)        {
+            matrix2[i] = parity.get(0,id[i]);
+        }
+        
+        if (matrix2.length != data.getRows()){
+            fail("The restored matrix must be of the same size as the original matris");
+        }
+
+        Matrix<Integer, Field<Integer>> toRestore = new Matrix<Integer, Field<Integer>>(data.getRows(), data.getCols(), field, matrix2);
+
+
+        Matrix<Integer, Field<Integer>> restored = Matrix.mult(cond, toRestore, field);
+
+//        System.out.println(cond);
+//
+//        System.out.println(toRestore);
+//
+//        System.out.println(restored);
+
+        assertEquals(restored, data);
+
+
+    }
+}
